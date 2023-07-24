30e9706b0d60fbf040ec84d2f4e2b4a460b8429b
Joseph Nasser <57519561+josephJaoNasser@users.noreply.github.com>
2023-07-24 15:38:41 +0800

    support passing token as query

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -3,35 +3,42 @@ const { failed } = require("../utils/response");
 const jwt = require("jsonwebtoken");
 
 const AuthenticationMiddleware = async (req, res, next) => {
-	const { authorization: authorizationHeader } = req.headers;
-	if (!authorizationHeader)
-		return res.status(401).json(failed("Please provide a valid authorization token."));
+  const { authorization: authorizationHeader } = req.headers;
+  const { access_token: queryToken } = req.query;
+  if (!authorizationHeader && !queryToken)
+    return res
+      .status(401)
+      .json(failed("Please provide a valid authorization token."));
 
-	const [,token] = authorizationHeader.split(" "); 
+  let token = authorizationHeader
+    ? authorizationHeader.split(" ")[1]
+    : queryToken;
 
-	if (!token)
-		return res.status(401).json(failed("Please provide a valid authorization token."));
-	
-	try {
-		const { JWT_SECRET_TOKEN } = process.env;
-		const data = jwt.verify(token, JWT_SECRET_TOKEN);
+  if (!token)
+    return res
+      .status(401)
+      .json(failed("Please provide a valid authorization token."));
 
-		const {
-			id,
-			access_token
-		} = data;
+  try {
+    const { JWT_SECRET_TOKEN } = process.env;
+    const data = jwt.verify(token, JWT_SECRET_TOKEN);
 
-		if (!id || !access_token) throw new Error("Wrong authorization token format.");
+    const { id, access_token } = data;
 
-		const authenticatedUser = await User.findOne({ userId: id });
+    if (!id || !access_token)
+      throw new Error("Wrong authorization token format.");
 
-		res.locals.user = authenticatedUser;
-		res.locals.access_token = access_token;
+    const authenticatedUser = await User.findOne({ userId: id });
 
-		next();
-	} catch(err) {
-		return res.status(401).json(failed("You have provided an expired or invalid token."));
-	}
-}
+    res.locals.user = authenticatedUser;
+    res.locals.access_token = access_token;
+
+    next();
+  } catch (err) {
+    return res
+      .status(401)
+      .json(failed("You have provided an expired or invalid token."));
+  }
+};
 
 module.exports = AuthenticationMiddleware;
